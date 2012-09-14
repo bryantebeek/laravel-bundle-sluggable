b2581888c7dec2b74e262fc53f7bfb2bf49919ab
Bryan te Beek <bryantebeek@gmail.com>
2012-09-14 17:32:44 +0200

    Add unit tests and refactor the sluggable class to allow testing
    Signed-off-by: Bryan te Beek <bryantebeek@gmail.com>

--- sluggable.php
+++ sluggable.php
@@ -12,94 +12,137 @@
 class Sluggable
 {
     /**
-     * Method that gets fired when the eloquent model is saved.
-     * Handles the slugging.
-     *
-     * @param  Model   $model
-     *
-     * @return bool
+     * @var null|\Laravel\Database\Eloquent\Model The model that is going to be sluggified is stored here.
      */
-    public static function make($model, $force = false)
+    private $model = null;
+    /**
+     * @var null|array The configuration array is stored here for internal use.
+     */
+    private $configuration = null;
+
+    /**
+     * @param  \Laravel\Database\Eloquent\Model      $model   The model that needs to be sluggified.
+     * @param bool                                   $force   Force the model to generate a new slug, usefull if 'on_update' needs to bypassed. Defaults to false.
+     * @param  array                                 $objects An array of {@link \Laravel\Database\Eloquent\Model} objects which the slug should be tested against, this defaults to null in which case Sluggable will check the database to determine the slug.
+     */
+    public static function make($model, $force = false, $objects = null)
     {
-        // skip if the model isn't sluggable
-        if (!isset($model::$sluggable))
+        $class = get_class($model);
+
+        // If the class has no sluggable configuration, there is nothing to be done so we stop.
+        if (!isset($class::$sluggable))
         {
-            return true;
+            return;
         }
 
-        $model_config = $model::$sluggable;
+        $instance = new Sluggable($model);
 
-        // read the default config:
-        // 1. application/config/sluggable.php
-        // 2. bundles/sluggable/config/sluggable.php
-
-        $default_config = Config::get('sluggable', Config::get('sluggable::sluggable', array()));
-
-        $config = array_merge($default_config, $model_config);
-
-        // nicer variables for readability
-        $build_from = $save_to = $style = $separator = $unique = $on_update = null;
-        extract($config, EXTR_IF_EXISTS);
-
-        // skip slug generation if the model exists or the slug field is already populated,
-        // and on_update is false ... unless we are forcing things!
-        if (!$force)
+        // Check if the model is going to be created or updated, if it's going to update
+        // and 'on_update' is false we can stop, unless we forced the update with the force parameter.
+        if (($model->exists && $instance->configuration['on_update'] == false) && !$force)
         {
-            if (($model->exists || !empty($model->{$save_to})) && !$on_update)
-            {
-                return true;
-            }
+            return;
         }
 
-        // build the slug string
-        if ($build_from)
+        // Create the slug
+        $slug = $instance->slug();
+
+        // Check if the slug is already present and weither we want to have unique slugs
+        if (($object = $instance->exists($slug, $objects)) && $instance->configuration['unique'])
         {
-            if (!is_array($build_from))
-            {
-                $build_from = array($build_from);
-            }
-            $string = '';
-            foreach ($build_from as $field)
-            {
-                $string .= $model->{$field}.' ';
-            }
+            // If the slug exists, we get the next available slug
+            $slug = $instance->next($slug, $object);
+        }
+
+        // We set the 'save_to' field of the model to the newly created slug, yeah!
+        $model->{$instance->configuration['save_to']} = $slug;
+    }
+
+    /**
+     * The constructor for Sluggable
+     *
+     * @param $model
+     */
+    protected function __construct($model)
+    {
+        $this->model = $model;
+        $this->configure();
+    }
+
+    /**
+     * This functions load our configuration in a few steps:
+     *
+     * 1. We get the configuration from the model
+     * 2. We get the 'sluggable' configuration from the main application
+     * 3. We get the 'sluggable' configuration from our bundle
+     *
+     * Then we merge the configurations and save them for later use.
+     */
+    protected function configure()
+    {
+        $class = get_class($this->model);
+        $model_configuration = $class::$sluggable;
+        $default_configuration = Config::get('sluggable', Config::get('sluggable::sluggable', array()));
+
+        $this->configuration = array_merge($default_configuration, $model_configuration);
+    }
+
+    /**
+     * Creates the actual slug from the 'build_from' field using the 'separator' configuration.
+     *
+     * @return string
+     */
+    protected function slug()
+    {
+        $string = $this->model->{$this->configuration['build_from']};
+        $separator = $this->configuration['separator'];
+        return Str::slug($string, $separator);
+    }
+
+    /**
+     * @param string $slug The slug which we are going to determine the existence of
+     * @param \Laravel\Database\Eloquent\Model $objects An array of {@link \Laravel\Database\Eloquent\Model} which we are going to test our slug against to see if it already exists.
+     *
+     * @return mixed If an object exists with the same slug, return it, otherwise return false.
+     */
+    protected function exists($slug, $objects = null)
+    {
+        $class = get_class($this->model);
+
+        if (!isset($objects))
+        {
+            // Get the objects with a slug like our preffered one and choose the one with the highest index prepended.
+            $object = $class::where($this->configuration['save_to'], 'LIKE', $slug.'%')->order_by(
+                $this->configuration['save_to'],
+                'DESC'
+            )->first();
+            return $object;
         } else
         {
-            $string = $model->__toString();
-        }
-
-        $string = trim($string);
-
-        // build slug using given slug style
-        switch ($config['style'])
-        {
-            case 'slug':
-            default:
-                $slug = Str::slug($string, $config['separator']);
-                break;
-        }
-
-        // check for uniqueness?
-        if ($unique)
-        {
-            $class = get_class($model);
-            $last = $class::where($save_to, 'LIKE', $slug.'%')->order_by($save_to, 'DESC')->first();
-
-            if ($last)
+            foreach ($objects as $object)
             {
-                $idx = substr($last->{$save_to}, strlen($slug));
-                $idx = ltrim($idx, $separator);
-                $idx = intval($idx);
-                $idx++;
-
-                $slug .= $separator.$idx;
+                if (strstr($object->{$this->configuration['save_to']}, $slug) !== false)
+                {
+                    return $object;
+                }
             }
         }
+        return null;
+    }
 
-        // update the slug field
-        $model->{$save_to} = $slug;
+    /**
+     * @param string $slug The current slug, which we are going to append an index to.
+     * @param \Laravel\Database\Eloquent\Model $object The object with the slug with the highest index
+     *
+     * @return string The newly calculated slug
+     */
+    protected function next($slug, $object)
+    {
+        $idx = substr($object->{$this->configuration['save_to']}, strlen($slug));
+        $idx = ltrim($idx, $this->configuration['separator']);
+        $idx = intval($idx);
+        $idx++;
 
-        // done!
-        return true;
+        return $slug .= $this->configuration['separator'].$idx;
     }
 }
---
+++ tests/sluggable.test.php
@@ -0,0 +1,127 @@
+<?php
+
+include(Bundle::path('sluggable').'sluggable.php');
+
+class Sluggable_Test extends PHPUnit_Framework_TestCase
+{
+    private $model = null;
+
+    public function setUp() {
+        $this->model = new Model();
+        $this->model->title = 'This is a test';
+        $this->model->other_title_field = 'This is another test';
+    }
+
+    public function testSlug()
+    {
+        Sluggable::make($this->model, false, array());
+        $this->assertEquals("this-is-a-test", $this->model->slug);
+    }
+
+    public function testExistsWithoutIndex()
+    {
+        $dummy = new Model();
+        $dummy->slug = 'this-is-a-test';
+
+        Sluggable::make($this->model, false, array($dummy));
+        $this->assertEquals("this-is-a-test-1", $this->model->slug);
+    }
+
+    public function testExistsWithIndex()
+    {
+        $dummy = new Model();
+        $dummy->slug = 'this-is-a-test-5';
+
+        Sluggable::make($this->model, false, array($dummy));
+        $this->assertEquals("this-is-a-test-6", $this->model->slug);
+    }
+
+    public function testOnUpdateTrue() {
+        $class = get_class($this->model);
+        $class::$sluggable['on_update'] = true;
+
+        $this->model->exists = true;
+        $this->model->slug = 'random';
+
+        Sluggable::make($this->model, false, array());
+        $this->assertEquals("this-is-a-test", $this->model->slug);
+
+        $class::$sluggable['on_update'] = null;
+    }
+
+    public function testOnUpdateFalse() {
+        $class = get_class($this->model);
+        $class::$sluggable['on_update'] = false;
+
+        $this->model->exists = true;
+        $this->model->slug = 'random';
+
+        Sluggable::make($this->model, false, array());
+        $this->assertEquals("random", $this->model->slug);
+
+        $class::$sluggable['on_update'] = null;
+    }
+
+    public function testForce() {
+        $class = get_class($this->model);
+        $class::$sluggable['on_update'] = false;
+
+        $this->model->exists = true;
+        $this->model->slug = 'random';
+
+        Sluggable::make($this->model, true, array());
+        $this->assertEquals("this-is-a-test", $this->model->slug);
+    }
+
+    public function testSaveTo() {
+        $class = get_class($this->model);
+        $class::$sluggable['save_to'] = 'other_slug_field';
+
+        Sluggable::make($this->model, false, array());
+        $this->assertEquals("this-is-a-test", $this->model->other_slug_field);
+
+        $class::$sluggable['save_to'] = 'slug';
+    }
+
+    public function testBuildFrom() {
+        $class = get_class($this->model);
+        $class::$sluggable['build_from'] = 'other_title_field';
+
+        Sluggable::make($this->model, false, array());
+        $this->assertEquals("this-is-another-test", $this->model->slug);
+
+        $class::$sluggable['build_from'] = 'title';
+    }
+
+    public function testSeparator() {
+        $class = get_class($this->model);
+        $class::$sluggable['separator'] = '/';
+
+        Sluggable::make($this->model, false, array());
+        $this->assertEquals("this/is/a/test", $this->model->slug);
+
+        $class::$sluggable['separator'] = '-';
+    }
+
+    public function testNotUnique() {
+        $class = get_class($this->model);
+        $class::$sluggable['unique'] = false;
+
+        $dummy = new Model();
+        $dummy->slug = 'this-is-a-test';
+
+        Sluggable::make($this->model, false, array($dummy));
+        $this->assertEquals("this-is-a-test", $this->model->slug);
+
+        $class::$sluggable['unique'] = true;
+    }
+
+    public function tearDown() {
+        $this->model = null;
+    }
+}
+
+class Model extends Eloquent
+{
+    public static $sluggable = array('build_from' => 'title');
+}
